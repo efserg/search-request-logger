840a6c4ed08f495279cd42116d1029812acc2d1d
Ефремов Сергей Юрьевич (ЗК-0000155) <sergey.y.efremov@bia-tech.ru>
2018-10-06 11:49:26 +0300

    Initial spring boot app

---
+++ src/main/java/space/efremov/searchrequestlogger/SearchRequestLoggerApplication.java
@@ -0,0 +1,12 @@
+package space.efremov.searchrequestlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchRequestLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchRequestLoggerApplication.class, args);
+	}
+}
