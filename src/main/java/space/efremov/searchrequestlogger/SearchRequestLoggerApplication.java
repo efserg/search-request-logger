3fc911450bb98d03e2907776c913add158f9f978
Sergey Y. Efremov <mynog@yandex.ru>
2018-10-09 19:00:18 +0300

    Add thymeleaf support

--- src/main/java/space/efremov/searchrequestlogger/SearchRequestLoggerApplication.java
+++ src/main/java/space/efremov/searchrequestlogger/SearchRequestLoggerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableMongoRepositories(basePackages = "space.efremov.searchrequestlogger.repository")
+@EnableWebMvc
 public class SearchRequestLoggerApplication {
 
 	public static void main(String[] args) {
