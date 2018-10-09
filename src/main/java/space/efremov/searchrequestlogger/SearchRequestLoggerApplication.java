75e9352ce92d51696823f8b6622eb466646ddb82
Sergey Y. Efremov <mynog@yandex.ru>
2018-10-09 19:19:27 +0300

    Merge branch 'try_integration'
    # Conflicts:
    #	src/main/java/space/efremov/searchrequestlogger/SearchRequestLoggerApplication.java

--- src/main/java/space/efremov/searchrequestlogger/SearchRequestLoggerApplication.java
+++ src/main/java/space/efremov/searchrequestlogger/SearchRequestLoggerApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableScheduling
